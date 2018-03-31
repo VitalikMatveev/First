958af2da2ca3933e3e4a31302b58bb6753d8e0db
VitalikMatveev <vitalik199902@gmail.com>
2018-03-31 18:54:26 +0300

    add Restoran add Bar add Human add Halle add Kitchen add Sex

---
+++ ConsoleApplication1/Restoran.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Restoran
+    {
+        public string Name { get; set; }
+        public string Adress { get; set; }
+        public List<Human> Owners { get; set; }
+        public Kitchen Kitchen { get; set; }
+        public List<Halle> Halles { get; set; }
+        public Bar Bar { get; set; }
+    }
+}
